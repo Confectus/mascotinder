9ab3b5ef88b1472bf9dbca33249f326414b80db9
SiilverShiine <francisco.encalada01@epn.edu.ec>
2022-08-24 16:26:57 -0500

    Edition PetDAO, was added the method getPetsByEmail()

--- source/src/main/java/model/dao/PetDAO.java
+++ source/src/main/java/model/dao/PetDAO.java
@@ -1,9 +1,11 @@
 package model.dao;
 
+import java.util.List;
+
 import model.entities.Pet;
 
 public interface PetDAO extends GenericDAO<Pet, Integer>{
 	
-	
+	public List<Pet> getPetsByOwnerEmail(String ownerEmail);
 	
 }
