e498d555fcb3c739ed4bc389c138eb611e5f4179
gianc <gianni.naranjo.15@gmail.com>
2022-08-24 16:32:36 -0500

    Mutiples changes

--- source/src/main/java/model/jpa/JPAPetDAO.java
+++ source/src/main/java/model/jpa/JPAPetDAO.java
@@ -13,42 +13,10 @@ public class JPAPetDAO extends JPAGenericDAO<Pet, Integer> implements PetDAO {
 	}
 
 	@Override
-	public void create(Pet instancia) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void update(Pet instancia) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void delete(Pet instancia) {
-		// TODO Auto-generated method stub
-
-	}
-	
-	/*
-	Check this part!!
-	@Override
-	public Pet getById(Integer id) {
-		// TODO Auto-generated method stub
+	public List<Pet> getPetsByOwnerEmail(String ownerEmail) {
+		
 		return null;
 	}
-
-	@Override
-	public Pet autorizar(String usuario, String password) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Pet> getAll() {
-		// TODO Auto-generated method stub
-		return null;
-	}*/
 
 
 }
