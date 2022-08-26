ce3104108c5264ddf0916927e08998d08111e42f
SiilverShiine <francisco.encalada01@epn.edu.ec>
2022-08-26 16:59:03 -0500

    were added pets and preferences in JPATableCreation

--- source/src/main/java/test/JPATableCreation.java
+++ source/src/main/java/test/JPATableCreation.java
@@ -4,6 +4,7 @@ import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Base64;
 
 import javax.imageio.ImageIO;
@@ -12,57 +13,58 @@ import model.dao.DAOFactory;
 import model.entities.Owner;
 import model.entities.Pet;
 import model.entities.PetImage;
+import model.entities.Preference;
 
 public class JPATableCreation {
 	
 	public static void main(String[] args)  {
 		
-		File file1 = new File("img/pelusin.jpg");
-		File file2 = new File("img/pelusin2.jpg");
-		File file3 = new File("img/pelusin3.jpg");
 		
-		System.out.println(file1.canRead());
+		ArrayList<File> files = new ArrayList<>();
+		files.add(new File("img/pelusin.jpg"));
+		files.add(new File("img/pelusin2.jpg"));
+		files.add(new File("img/pelusin3.jpg"));
 		
-		System.out.println(file1.getAbsolutePath());
+		ArrayList<Owner> owners = new ArrayList<>();
+		owners.add(new Owner("francisco.encalada01@epn.edu.ec", "Francisco", "rafa123123"));
+		owners.add(new Owner("ejemplo@gmail.com", "Carlos Iñiguez", "123"));
 		
-		Owner owner1 = new Owner("francisco.encalada01@epn.edu.ec", "Francisco", "rafa123123");		
-		DAOFactory.getFactory().getOwnerDAO().create(owner1);
-		
-		Pet pt = new Pet(1,"Alpha","x","y",12,owner1);
-		Pet pt2 = new Pet(2,"Betha","x","y",12,owner1);
-		DAOFactory.getFactory().getPetDAO().create(pt);
-		DAOFactory.getFactory().getPetDAO().create(pt2);
+		for (Owner owner : owners) {
+			DAOFactory.getFactory().getOwnerDAO().create(owner);
+		}
 		
 		
+		String[] sex = {"male","female"};
+		for (int i=0;i<15;i++) {
+			Pet pet = new Pet(null, "mascota" + 1, "a", "y", 12 + i, owners.get(i%2));
+			DAOFactory.getFactory().getPetDAO().create(pet);
+			
+			PetImage image = new PetImage(null, fileToBase64String(files.get(i%3)), pet);
+			DAOFactory.getFactory().getPetImageDAO().create(image);
+			
+			Preference preference = new Preference(null, "dog", sex[i % 2], i % 3 + 5, i % 3 + 8, pet);
+			DAOFactory.getFactory().getPreferenceDAO().create(preference);
+		}
+		
+				
+	}
+	
+	public static String fileToBase64String(File file) {
+		
+		BufferedImage bi;
 		try {
-			PetImage image1 = new PetImage(1,fileToBase64String(file1),pt);
-			PetImage image2 = new PetImage(1,fileToBase64String(file2),pt);
-			PetImage image3 = new PetImage(1,fileToBase64String(file3),pt);
-			
-			DAOFactory.getFactory().getPetImageDAO().create(image1);
-			DAOFactory.getFactory().getPetImageDAO().create(image2);
-			DAOFactory.getFactory().getPetImageDAO().create(image3);
-			
+			bi = ImageIO.read(file);
+			ByteArrayOutputStream output = new ByteArrayOutputStream();
+			ImageIO.write(bi, "jpg", output);
+			String imageAsBase64 = Base64.getEncoder().encodeToString(output.toByteArray());
+			return imageAsBase64;
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
+		return null;
 		
-		
-		
-				
-	}
-	
-	public static String fileToBase64String(File file) throws IOException {
-		
-		BufferedImage bi = ImageIO.read(file);
-		ByteArrayOutputStream output = new ByteArrayOutputStream();
-		ImageIO.write(bi, "jpg", output);
-		
-		String imageAsBase64 = Base64.getEncoder().encodeToString(output.toByteArray());
-		
-		return imageAsBase64;
 	}
 	
 }
